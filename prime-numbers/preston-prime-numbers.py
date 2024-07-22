1a95d836be73d2e084f9113117cf251989413830
Alexandru Pop <alexpop89@users.noreply.github.com>
2024-07-22 21:45:53 +0300

    Add duration to prime

--- prime-numbers/preston-prime-numbers.py
+++ prime-numbers/preston-prime-numbers.py
@@ -1,3 +1,16 @@
+from datetime import datetime
+
+def pretty_print_duration(start_timestamp, end_timestamp):
+    duration = end_timestamp - start_timestamp
+    total_seconds = duration.total_seconds()
+    milliseconds = duration.microseconds
+
+    hours = int(total_seconds // 3600)
+    minutes = int((total_seconds % 3600) // 60)
+    seconds = int(total_seconds % 60)
+
+    print(f"{hours} hours, {minutes} minutes, {seconds} seconds, {milliseconds} milliseconds")
+
 def is_prime(input_number):
     for i in range(2, input_number):
         if input_number % i == 0:
@@ -5,6 +18,8 @@ def is_prime(input_number):
     return True
     
 number_of_prime_numbers = int(input("Enter the number of prime numbers you want to print: "))
+start_time = datetime.now()
+
 count = 1
 current_number = 3
 
@@ -15,3 +30,7 @@ while count < number_of_prime_numbers:
         print(current_number)
         count += 1
     current_number += 1
+
+end_time = datetime.now()
+
+pretty_print_duration(start_time, end_time)
